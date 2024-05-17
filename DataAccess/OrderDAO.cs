005e1ac587c099489ace80752678f53fca95b732
Dang Thanh <16thanh2003@gmail.com>
2024-05-17 14:41:13 +0700

    Add functions to OrderDAO

--- DataAccess/OrderDAO.cs
+++ DataAccess/OrderDAO.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using BusinessObject;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.ConstrainedExecution;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,5 +10,41 @@ namespace DataAccess
 {
     public class OrderDAO
     {
+        //Singleton pattern
+        private static OrderDAO instance = null;
+        private static readonly object instanceLock = new object();
+        private OrderDAO() { }
+        public static OrderDAO Instance
+        {
+            get
+            {
+                lock (instanceLock)
+                {
+                    if (instance == null)
+                    {
+                        instance = new OrderDAO();
+                    }
+                    return instance;
+                }
+            }
+        }
+
+        //Get list of orders
+        public IEnumerable<Order> GetOrderList()
+        {
+            List<Order> orders = new List<Order>();
+            try
+            {
+                var myStoreDB = new MyStoreContext();
+                orders = myStoreDB.Orders.ToList();
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+            return orders;
+        }
+
+
     }
 }
