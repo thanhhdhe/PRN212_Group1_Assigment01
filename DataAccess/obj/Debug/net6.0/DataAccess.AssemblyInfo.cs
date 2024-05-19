ee7b1d5cf4206ab9e218a842b4ab649f0fa88893
Dang Thanh <16thanh2003@gmail.com>
2024-05-19 20:45:50 +0700

    Style the sidebar

--- DataAccess/obj/Debug/net6.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net6.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49a76dee97baae2aec80494dc6c3d48ba2742f8a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
