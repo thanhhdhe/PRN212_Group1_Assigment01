0db461851f0d61aef0e6d0f8c266db251564af4b
Lê Minh Thắng <107106191+lethangd@users.noreply.github.com>
2024-05-19 15:56:35 +0700

    .

---
+++ MyStoreWPFApp/AdminDashboard.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyStoreWPFApp
+{
+	/// <summary>
+	/// Interaction logic for AdminDashboard.xaml
+	/// </summary>
+	public partial class AdminDashboard : Window
+	{
+		public AdminDashboard()
+		{
+			InitializeComponent();
+			MainFrame.Navigate(new HomePage());
+		}
+		private void NavigateToHome(object sender, RoutedEventArgs e)
+		{
+			MainFrame.Navigate(new HomePage());
+		}
+
+		private void NavigateToProducts(object sender, RoutedEventArgs e)
+		{
+			MainFrame.Navigate(new ProductsPage());
+		}
+
+		private void NavigateToOrders(object sender, RoutedEventArgs e)
+		{
+			MainFrame.Navigate(new OrdersPage(0));
+		}
+
+		private void NavigateToStaff(object sender, RoutedEventArgs e)
+		{
+			MainFrame.Navigate(new StaffPage());
+		}
+
+		private void LogoutButton_Click(object sender, RoutedEventArgs e)
+		{
+		}
+	}
+}
---
+++ MyStoreWPFApp/OrderDetailWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using BusinessObject.Models;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MyStoreWPFApp
+{
+    /// <summary>
+    /// Interaction logic for OrderDetailWindow.xaml
+    /// </summary>
+    public partial class OrderDetailWindow : Window
+    {
+		public IEnumerable<OrderDetail> OrderDetails { get; set; }
+
+		public OrderDetailWindow(IEnumerable<OrderDetail> orderDetails)
+		{
+			InitializeComponent();
+			OrderDetails = orderDetails ?? new List<OrderDetail>();
+			OrderDetailsDataGrid.ItemsSource = OrderDetails;
+		}
+	}
+}
